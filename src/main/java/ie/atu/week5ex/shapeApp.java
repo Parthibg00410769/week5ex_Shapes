dea0b50a56a7b63e9432c8bb9edb7a9848202ffc
Parthibg00410769 <g00410769@atu.ie>
2022-10-17 22:53:31 +0100

    Added new file new method

--- src/main/java/ie/atu/week5ex/shapeApp.java
+++ src/main/java/ie/atu/week5ex/shapeApp.java
@@ -6,12 +6,30 @@ public class shapeApp {
         System.out.println("Please enter the length of the rectangle: ");
         Scanner input = new Scanner(System.in);
         double length = input.nextDouble();
-        System.out.println("You entered : "+length);
+        Rectangle shapeRect = new Rectangle();
+        shapeRect.setLength(length);
+        System.out.println("You entered : "+shapeRect.getLength());
+
+
 
         System.out.println("Please enter the width of the rectangle: ");
-
         double width = input.nextDouble();
-        System.out.println("You entered : "+width);
+        shapeRect.setWidth(width);
+        System.out.println("You entered : "+shapeRect.getWidth());
+
+        //
+        Rectangle shapeRect2 = new Rectangle();
+        System.out.println("Please enter the second length of the rectangle: ");
+        double length2 = input.nextDouble();
+        shapeRect2.setLength(length2);
+        System.out.println("You entered : "+shapeRect2.getLength());
+
+
+
+        System.out.println("Please enter the second width of the rectangle: ");
+        double width2 = input.nextDouble();
+        shapeRect2.setWidth(width2);
+        System.out.println("You entered : "+shapeRect2.getWidth());
 
     }
 }
